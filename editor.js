726233b04d8f2bae67d31c2edd867f24bcdff247
blaze3458 <furkanuguz3458@gmail.com>
2018-10-17 19:02:55 +0300

    first commit

---
+++ editor.js
@@ -0,0 +1,16 @@
+document.getElementsByClassName("editor")[0].addEventListener("input", function(event) {
+    console.log(event);
+	if(event.target.innerHTML.length === 0){
+		event.target.innerHTML = "<p><br></p>";
+	}
+}, false);
+
+document.getElementsByClassName("icon-undo")[0].addEventListener("click", function(event){
+	console.log(event);
+	document.execCommand("undo",false,null);
+},false);
+
+document.getElementsByClassName("icon-redo")[0].addEventListener("click", function(event){
+	console.log(event);
+	document.execCommand("redo",false,null);
+},false);
